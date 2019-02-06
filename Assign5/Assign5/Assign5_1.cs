fbe00b8f69e008722c8214b9a77b7c5725073393
briannadrew <briannadrew@trentu.ca>
2019-02-05 22:35:06 -0500

    commit

---
+++ Assign5/Assign5/Assign5_1.cs
@@ -0,0 +1,99 @@
+﻿/* Author: Brianna Drew
+Student ID: 0622446
+Description: This is a program that uses another class and objects to perform math involving fractions.
+
+Data Dictionary:
+ * den: int variable that will hold the number inputted by the user to be the denominator of the object fraction2.
+ * fraction1: object of the Fraction class that is, obviously, a fraction.
+ * fraction2: object of the Fraction class that is, obviously, a fraction.
+ * fraction3: object of the Fraction class that is, obviously, a fraction.
+ * num: int variable that will hold the number inputted by the user to be the numerator of the object fraction2.
+ * response: char variable that will hold whether or not the user wants to run through the program again ('Y'/'y' for Yes or 'N'/'n' for No). */
+
+using System;
+public static class Driver
+{
+    public static void Main()
+    {
+        // intro to program.
+        Console.WriteLine("Welcome to the Fraction Math Program!");
+        // defining variable and data type.
+        char response;
+
+        do
+        {
+            // defining variables and data type.
+            int num, den;
+            // create first fraction object using first (no parameter) constructor.
+            Fraction fraction1 = new Fraction();
+            // create third fraction object (not using a constructor).
+            Fraction fraction3;
+
+            // prompt the user to enter numbers for the numerator and the denominator of two fractions (4 seperate numbers),
+            // using properties for the first fraction and storing them in variables for the second fraction.
+            Console.WriteLine(" ");
+            Console.Write("Enter a numerator for the first fraction: ");
+            fraction1.Numerator = (Convert.ToInt32(Console.ReadLine()));
+            Console.Write("Enter a denominator for the first fraction: ");
+            fraction1.Denominator = (Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine(" ");
+            Console.Write("Enter a numerator for the second fraction: ");
+            num = (Convert.ToInt32(Console.ReadLine()));
+            Console.Write("Enter a denominator for the second fraction: ");
+            den = (Convert.ToInt32(Console.ReadLine()));
+
+            // create second fraction object using second (two parameters, using inputted values from above stored in variables num and den) constructor.
+            Fraction fraction2 = new Fraction(num, den);
+
+            // printing out the first two fractions.
+            Console.WriteLine(" ");
+            Console.WriteLine("The first fraction: {0}", fraction1.ToString());
+            Console.WriteLine("The second fraction: {0}", fraction2.ToString());
+            Console.WriteLine(" ");
+
+            // multiplying the first two fractions using overloaded operator and assigning that resulting fraction as the third fraction object.
+            fraction3 = fraction1 * fraction2;
+            // printing out the equation.
+            Console.WriteLine("{0} x {1} = {2}", fraction1.ToString(), fraction2.ToString(), fraction3.ToString());
+
+            // adding the first two fractions using overloaded operator and assigning that resulting fraction as the third fraction object.
+            fraction3 = fraction1 + fraction2;
+            // printing out the equation.
+            Console.WriteLine("{0} + {1} = {2}", fraction1.ToString(), fraction2.ToString(), fraction3.ToString());
+            Console.WriteLine(" ");
+
+            // print out whether the first fraction is greater than or equal to the second fraction or not (using overloaded operator).
+            if (fraction1 >= fraction2)
+            {
+                Console.WriteLine("{0} IS greater than or equal to {1}", fraction1.ToString(), fraction2.ToString());
+            }
+            else
+            {
+                Console.WriteLine("{0} is NOT greater than or equal to {1}", fraction1.ToString(), fraction2.ToString());
+            }
+
+            // print out whether the first fraction is less than or equal to the second fraction or not (using overloaded operator).
+            if (fraction1 <= fraction2)
+            {
+                Console.WriteLine("{0} IS less than or equal to {1}", fraction1.ToString(), fraction2.ToString());
+            }
+            else
+            {
+                Console.WriteLine("{0} is NOT less than or equal to {1}", fraction1.ToString(), fraction2.ToString());
+            }
+
+
+            do
+            {
+                // prompt user to enter whether they want to do the program again or not.
+                Console.WriteLine(" ");
+                Console.WriteLine("Do you want to restart? Type 'Y' for yes or 'N' for no.");
+                response = (Convert.ToChar(Console.ReadLine()));
+            } while (response != 'N' && response != 'n' && response != 'Y' && response != 'y'); // error check to keep asking until user enters a valid response.
+
+        } while (response != 'N' && response != 'n'); // while the user still wants to enter more fractions and have math done with them, keep basically restarting
+        // the program when it reaches the "end".
+
+        Console.ReadLine();
+    }
+}
---
+++ Assign5/Assign5/Assign5_2.cs
@@ -0,0 +1,197 @@
+﻿/* Author: Brianna Drew
+Student ID: 0622446
+Description: This is the class (driven by the class "Driver") to basically take care of the mathematics and verifications behind the fractions and of course construct
+the fractions into objects, the properties being the numerator and the denominator.
+
+Data Dictionary:
+ * added: object created in the + overloaded operator to be the fraction that is the sum of the first two fractions.
+ * den: int variable parameter passed to the second Fraction() constructor from the Driver class to hold the denominator of the fraction.
+ * denominator: private int variable that represents the denominator of the fraction object.
+ * fract1: parameter representing the first fraction object used in all overloaded operators to use it's numerator and denominator properties (assigning and
+ *         calculating).
+ * fract2: parameter representing the second fraction object used in all overloaded operators to use it's numerator and denominator properties (assigning and
+ *         calculating).
+ * GCD: int variable that will hold the greatest common denominator of two fractions calculated in the Reduce() method.
+ * multipication: object created in the * overloaded operator to be the fraction that is the product of the first two fractions.
+ * newnum1: int variable used in the + overloaded operator that will hold the first number calculated to be used to calculate the numerator of the fraction
+ *          representing the sum of the first two fractions.
+ * newnum2: int variable used in the + overloaded operator that will hold the second number calculated to be used to calculate the numerator of the fraction
+ *          representing the sum of the first two fractions.
+ * num: int variable parameter passed to the second Fraction() constructor from the Driver class to hold the numerator of the fraction.
+ * numerator: private int variable that represents the numerator of the fraction object.
+ * val1: double variable used in the >= and <= overloaded operators to hold the calculated floating point value of the first fraction to be compared to the second
+ *       fraction's floating point value to return a boolean value. 
+ * val2: double variable used in the >= and <= overloaded operators to hold the calculated floating point value of the second fraction to be compared to the first
+ *       fraction's floating point value to return a boolean value. */
+
+using System;
+public class Fraction
+{
+    // defining variables and their data types.
+    private int numerator;
+    private int denominator;
+
+    // no parameter constructor.
+    public Fraction()
+    {
+        // setting the values of variables.
+        numerator = 0;
+        denominator = 1;
+    }
+
+    // two parameter constructor.
+    // parameters: num representing the user-inputted numerator value and den representing the user-inputted denominator value.
+    public Fraction(int num, int den)
+    {
+        // setting the values of variables to the values passed from Driver class.
+        numerator = num;
+        // if the denominator is 0, change it to 1 so there is no error.
+        if (den == 0)
+        {
+            denominator = 1;
+        }
+        else
+        {
+            denominator = den;
+        }
+        // reduce the fraction to simplest form.
+        Reduce();
+    }
+
+    // numerator property.
+    public int Numerator
+    {
+        // setting the numerator property to the given value.
+        get
+        { return numerator; }
+        set
+        { numerator = value;}
+    }
+
+    // denominator property.
+    public int Denominator
+    {
+        // setting the denominator property to the given value.
+        get
+        { return denominator; }
+        set
+        {
+            // if the denominator is 0, change it to 1 so there is no error.
+            if (value == 0)
+            {
+                denominator = 1;
+            }
+            else
+            {
+                denominator = value;
+            }
+            // reduce the fraction to simplest form.
+            Reduce();
+        }
+        
+    }
+
+    // reduce method.
+    private void Reduce()
+    {
+        int GCD = 0;
+        // find the greatest common divider by calculating remainders.
+        for (int x = 1; x <= denominator; x++)
+        {
+            if ((numerator % x == 0) && (denominator % x == 0))
+                GCD = x;
+        }
+        // if there is a remainder, simplify the fraction by dividing both numbers by the GCD.
+        if (GCD != 0)
+        {
+            numerator = numerator / GCD;
+            denominator = denominator / GCD;
+        }
+    }
+
+    // to string method.
+    public override string ToString()
+    {
+        // create a string to represent the fraction to be printed out in the Driver class.
+        return numerator + "/" + denominator;
+    }
+
+    // multiply operator.
+    // parameters: fract1 representing the first fraction object and fract2 representing the second fraction object.
+    public static Fraction operator*(Fraction fract1, Fraction fract2)
+    {
+        // create new fraction object to represent the product of the first two fractions.
+        Fraction multipication = new Fraction();
+
+        // multiply the two numerators and the two denominators of the fractions together to get the new numerator and denominator of the fraction that represents
+        // the product of the first two fractions.
+        multipication.numerator = fract1.numerator * fract2.numerator;
+        multipication.denominator = fract1.denominator * fract2.denominator;
+
+        // reduce the calculated fraction to it's simplest form.
+        multipication.Reduce();
+
+        return multipication;
+    }
+
+    // add operator.
+    // parameters: fract1 representing the first fraction object and fract2 representing the second fraction object.
+    public static Fraction operator+(Fraction fract1, Fraction fract2)
+    {
+        // create new fraction object to represent the sum of the first two fractions.
+        Fraction added = new Fraction();
+
+        // cross-multiply the fractions to get two numbers to be added together to be the new numerator of the fraction that represents the sum of the first two
+        // fractions.
+        int newnum1 = fract1.numerator * fract2.denominator;
+        int newnum2 = fract2.numerator * fract1.denominator;
+        added.numerator = newnum1 + newnum2;
+
+        // multiply the two denominators of the fractions together to get the new denominator of the fraction that represents the sum of the first two fractions.
+        added.denominator = fract1.denominator * fract2.denominator;
+
+        // reduce the calcuated fraction to it's simplest form.
+        added.Reduce();
+
+        return added;
+    }
+
+    // greater than or equal operator.
+    // parameters: fract1 representing the first fraction object and fract2 representing the second fraction object.
+    public static bool operator>=(Fraction fract1, Fraction fract2)
+    {
+        // calculate floating point value of each of the first two fractions.
+        double val1 = (double)fract1.numerator / fract1.denominator;
+        double val2 = (double)fract2.numerator / fract2.denominator;
+
+        // compare the floating point values and return true if the first is less than or equal to the second, false if it is not.
+        if (val1 >= val2)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    // less than or equal operator.
+    // parameters: fract1 representing the first fraction object and fract2 representing the second fraction object.
+    public static bool operator<=(Fraction fract1, Fraction fract2)
+    {
+        // calculate floating point value of each fraction.
+        double val1 = (double)fract1.numerator / fract1.denominator;
+        double val2 = (double)fract2.numerator / fract2.denominator;
+
+        // compare the floating point values and return true if the first is less than or equal to the second, false if it is not.
+        if (val1 <= val2)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+}
